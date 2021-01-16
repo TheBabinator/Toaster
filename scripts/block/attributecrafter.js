3d489196ebb411a3bfcbd6af3b42437f2fd9de9b
TheBabinator <Sebastiaan.Boom@outlook.com>
2021-01-16 22:42:56 +0000

    geothermal boiler

---
+++ scripts/block/attributecrafter.js
@@ -0,0 +1,15 @@
+function extend(name) {
+    const content = extendContent(AttributeSmelter, name, {
+        
+    });
+    content.buildType = () => extendContent(AttributeSmelter.AttributeSmelterBuild, content, {
+        draw() {
+            Draw.rect(content.region, this.tile.drawx(), this.tile.drawy());
+        }
+    });
+    return content;
+}
+
+module.exports = {
+    extend: extend,
+}
--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,2 +1,5 @@
+const attributeCrafter = require("block/attributecrafter")
 const turbineGenerator = require("block/turbinegenerator")
+
+const geothermalBoiler = attributeCrafter.extend("geothermal-boiler");
 const advancedTurbine = turbineGenerator.extend("advanced-turbine");
